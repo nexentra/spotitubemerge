949c112a65c989e420d8c8c1a851c6d9993a7cf6
towhid <ktowhid20@gmail.com>
2023-07-20 14:00:45 +0600

    front page under development

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,10 +1,22 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Link from "next/link";
+import Head from "next/head";
+import Navbar from "@/components/ui/navbar";
 
 export default function Home() {
   return (
     <>
+    <Head>
+        <title>Spotitubemerge - Merge your playlists in few clicks!!</title>
+        <meta
+          name="description"
+          content="Spotitubemerge is a simple web app that allows you to merge your Spotify and Youtube playlists in few clicks!!"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar/>
       
     </>
   );
