fd37ce11b134170be4c44595d131f560399a3845
towhid <ktowhid20@gmail.com>
2023-09-13 01:17:09 +0600

    seperated merger handler into different functions

--- internal/combine/api.go
+++ internal/combine/api.go
@@ -18,7 +18,7 @@ import (
 )
 
 func RegisterHandlers(r *echo.Group, authenticator *spotifyauth.Authenticator, config *oauth2.Config, errorLog *log.Logger, infoLog *log.Logger, env map[string]string, redisClient *redis.Client) {
-	res := Resource{
+	res := &Resource{
 		Authenticator: authenticator,
 		ErrorLog:      errorLog,
 		InfoLog:       infoLog,
@@ -46,7 +46,12 @@ type Resource struct {
 	RedisClient   *redis.Client
 }
 
-func (r Resource) mergeYtSpotify(c echo.Context) error {
+type AllYoutubeItems struct {
+	Title string `json:"title"`
+	Id    string `json:"id"`
+}
+
+func (r *Resource) mergeYtSpotify(c echo.Context) error {
 	//get data
 	mergerList := MergerList{}
 	if err := c.Bind(&mergerList); err != nil {
@@ -54,6 +59,66 @@ func (r Resource) mergeYtSpotify(c echo.Context) error {
 	}
 
 	//get auth headers items
+	authHeaderTypeSpotify, _, err := getAuthHeaders(c)
+	if err != nil {
+		r.ErrorLog.Printf("Failed task: %v", err)
+	}
+
+	//spotify client
+	spotifyClient, user, err := r.createSpotifyClientAndUser(c, authHeaderTypeSpotify)
+	if err != nil {
+		r.ErrorLog.Printf("Failed task: %v", err)
+	}
+
+	//get spotify items
+	allSpotifyTitles,err := r.getAllSpotifyItems(c, mergerList, spotifyClient)
+	if err != nil {
+		r.ErrorLog.Printf("Failed task: %v", err)
+	}
+
+	//create youtube client and service
+	ytClient, service, err := r.createYoutubeClientAndService(c)
+	if err != nil {
+		r.ErrorLog.Printf("Failed task: %v", err)
+	}
+
+
+	// Get YouTube items
+	allYoutubeTitles,err := r.getAllYoutubeItems(mergerList, service)
+	if err != nil {
+		r.ErrorLog.Printf("Failed task: %v", err)
+	}
+
+	// Create a new spotify playlist
+	newSpotifyPlaylist, err := spotifyClient.CreatePlaylistForUser(context.Background(), user.ID, "spotitubeMergePlaylist", "New playlist for searched tracks", false, false)
+	if err != nil {
+		r.ErrorLog.Printf("Error creating playlist: %v", err)
+	}
+
+	// Create a new youtube playlist
+	newYoutubePlaylist, err := ytplaylist.CreatePlaylist(ytClient, "spotitubeMergePlaylist")
+	if err != nil {
+		r.ErrorLog.Printf("Error creating playlist: %v", err)
+	}
+
+	// Add tracks to the spotify playlist
+	err = r.addTracksToSpotifyPlaylist(allYoutubeTitles,spotifyClient, newSpotifyPlaylist, ytClient,newYoutubePlaylist)
+	if err != nil {
+		r.ErrorLog.Printf("Failed task: %v", err)
+	}
+
+	//Add tracks to the youtube playlist
+	err = r.addTracksToYoutubePlaylist(allSpotifyTitles,spotifyClient, newSpotifyPlaylist, ytClient,newYoutubePlaylist)
+	if err != nil {
+		r.ErrorLog.Printf("Failed task: %v", err)
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"items": allYoutubeTitles,
+	})
+}
+
+func getAuthHeaders(c echo.Context) (*oauth2.Token, *oauth2.Token, error) {
 	var authHeaderTypeYoutube *oauth2.Token
 	var authHeaderTypeSpotify *oauth2.Token
 	authHeaderYoutube := c.Request().Header.Get("AuthorizationYoutube")
@@ -61,26 +126,29 @@ func (r Resource) mergeYtSpotify(c echo.Context) error {
 	json.Unmarshal([]byte(authHeaderSpotify), &authHeaderTypeSpotify)
 	json.Unmarshal([]byte(authHeaderYoutube), &authHeaderTypeYoutube)
 	if authHeaderTypeSpotify == nil || authHeaderTypeYoutube == nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{
-			"error": "no auth header",
-		})
+		return nil, nil, fmt.Errorf("no auth header")
 	}
+	return authHeaderTypeSpotify, authHeaderTypeYoutube , nil
+}
 
-	//spotify client
-	client := spotify.New(r.Authenticator.Client(c.Request().Context(), authHeaderTypeSpotify))
+func (r *Resource) createSpotifyClientAndUser(c echo.Context, authHeaderTypeSpotify *oauth2.Token) (*spotify.Client, *spotify.PrivateUser, error){
+	spotifyClient := spotify.New(r.Authenticator.Client(c.Request().Context(), authHeaderTypeSpotify))
 
 	//get spotify user
-	user, err := client.CurrentUser(c.Request().Context())
+	user, err := spotifyClient.CurrentUser(c.Request().Context())
 	if err != nil {
-		r.ErrorLog.Print(err)
+		return nil, nil, fmt.Errorf("couldn't get user: %w", err)
 	}
 
-	//get spotify items
+	return spotifyClient, user, err
+}
+
+func (r *Resource) getAllSpotifyItems(c echo.Context, mergerList MergerList, spotifyClient *spotify.Client)([]spotify.PlaylistItemTrack, error){
 	var allSpotifyTitles []spotify.PlaylistItemTrack
 	for _, playlist := range mergerList.SpotifyPlaylists {
-		playlistItems, err := client.GetPlaylistItems(c.Request().Context(), spotify.ID(playlist))
+		playlistItems, err := spotifyClient.GetPlaylistItems(c.Request().Context(), spotify.ID(playlist))
 		if err != nil {
-			r.ErrorLog.Print(err)
+			return nil, fmt.Errorf("couldn't get playlist items: %w", err)
 		}
 		for _, playlistItems := range playlistItems.Items {
 			allSpotifyTitles = append(allSpotifyTitles, playlistItems.Track)
@@ -95,17 +163,21 @@ func (r Resource) mergeYtSpotify(c echo.Context) error {
 		fmt.Println("title: ", title.Track.Name)
 	}
 
+	
+	return allSpotifyTitles, nil
+}
+
+func (r *Resource) createYoutubeClientAndService(c echo.Context)(*http.Client, *youtube.Service, error){
 	ytClient := c.Get("client").(*http.Client)
 	service, err := youtube.New(ytClient)
 	if err != nil {
-		return err
+		return nil, nil, fmt.Errorf("Error creating new YouTube client: %v", err)
 	}
 
-	// Get YouTube items
-	type AllYoutubeItems struct {
-		Title string `json:"title"`
-		Id    string `json:"id"`
-	}
+	return ytClient, service, err
+}
+
+func (r *Resource) getAllYoutubeItems(mergerList MergerList, service *youtube.Service)([]AllYoutubeItems, error){
 	var allYoutubeTitles []AllYoutubeItems
 	for _, playlist := range mergerList.YoutubePlaylists {
 		call := service.PlaylistItems.List([]string{"snippet"}).
@@ -120,7 +192,7 @@ func (r Resource) mergeYtSpotify(c echo.Context) error {
 
 			response, err := call.Do()
 			if err != nil {
-				fmt.Print(err.Error())
+				return nil, fmt.Errorf("Error fetching playlist items: %v", err)
 				break
 			}
 
@@ -136,24 +208,16 @@ func (r Resource) mergeYtSpotify(c echo.Context) error {
 		}
 	}
 
-	// Create a new spotify playlist
-	newSpotifyPlaylist, err := client.CreatePlaylistForUser(context.Background(), user.ID, "spotitubeMergePlaylist", "New playlist for searched tracks", false, false)
-	if err != nil {
-		fmt.Println("Error creating playlist:", err)
-	}
+	return allYoutubeTitles, nil
+}
 
-	newYoutubePlaylist, err := ytplaylist.CreatePlaylist(ytClient, "spotitubeMergePlaylist")
-	if err != nil {
-		fmt.Println("Error creating playlist: ", err)
-	}
-
-	// Add tracks to the spotify playlist
+func (r *Resource) addTracksToSpotifyPlaylist(allYoutubeTitles []AllYoutubeItems,spotifyClient *spotify.Client, newSpotifyPlaylist *spotify.FullPlaylist, ytClient *http.Client,newYoutubePlaylist *youtube.Playlist ) error {
 	for i, items := range allYoutubeTitles {
 		fmt.Println("index: ", i)
 		fmt.Println("title: ", items.Title)
 
 		// Search for the video in Spotify
-		results, err := client.Search(context.Background(), items.Title, spotify.SearchTypeTrack)
+		results, err := spotifyClient.Search(context.Background(), items.Title, spotify.SearchTypeTrack)
 		if err != nil {
 			fmt.Println("Error searching for video:", err)
 			continue
@@ -164,7 +228,7 @@ func (r Resource) mergeYtSpotify(c echo.Context) error {
 			firstTrack := results.Tracks.Tracks[0]
 
 			// Add the track to the spotify playlist
-			_, err := client.AddTracksToPlaylist(context.Background(), newSpotifyPlaylist.ID, firstTrack.ID)
+			_, err := spotifyClient.AddTracksToPlaylist(context.Background(), newSpotifyPlaylist.ID, firstTrack.ID)
 			if err != nil {
 				fmt.Println("Error adding track to playlist:", err)
 			} else {
@@ -183,11 +247,15 @@ func (r Resource) mergeYtSpotify(c echo.Context) error {
 		}
 	}
 
+	return nil
+}
+
+func (r *Resource) addTracksToYoutubePlaylist(allSpotifyTitles []spotify.PlaylistItemTrack,spotifyClient *spotify.Client, newSpotifyPlaylist *spotify.FullPlaylist, ytClient *http.Client,newYoutubePlaylist *youtube.Playlist ) error {
 	for i, title := range allSpotifyTitles {
 		fmt.Println("index: ", i)
 		fmt.Println("title: ", title.Track.Artists[0].Name)
 		// Add the track to the newly created playlist
-		_, err := client.AddTracksToPlaylist(context.Background(), newSpotifyPlaylist.ID, title.Track.ID)
+		_, err := spotifyClient.AddTracksToPlaylist(context.Background(), newSpotifyPlaylist.ID, title.Track.ID)
 		if err != nil {
 			fmt.Println("Error adding track to playlist:", err)
 		} else {
@@ -208,10 +276,7 @@ func (r Resource) mergeYtSpotify(c echo.Context) error {
 		} else {
 			fmt.Println("Video added to YouTube playlist:", title.Track.Name)
 		}
-
 	}
 
-	return c.JSON(http.StatusOK, echo.Map{
-		"items": allYoutubeTitles,
-	})
+	return nil
 }
